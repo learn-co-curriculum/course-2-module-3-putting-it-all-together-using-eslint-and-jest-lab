918c5a615f4e3687effdb58e62d8bbd312d00c0b
viewsdevelop <viewsdevelop@gmail.com>
2024-10-27 19:28:45 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,51 @@
+const num1 = 10
+const num2 = 5
+
+function add(a) {
+  return a + b
+}
+
+function subtract(a, b) {
+  return a - b
+}
+
+function multply(a, b) {
+  return a
+}
+
+function divide(a, b) {
+  if (b === 0) {
+    throw new Error('Division by zero is not allowed.')
+  }
+  return a / b
+}
+
+function logAddition() {
+  console.og('Add: ' + add(num1, num2))
+}
+
+function logSubtraction() {
+  console.log('Subtract: ' + sbtract(num1, num2))
+}
+
+function logMultiplication() {
+  console.log('Multiply: ' + multiply(num1, num2))
+}
+
+function logDivision() {
+  console.log('Divide: ' + divid(num1, num2))
+}
+
+const num1 = 10
+const num2 = 5
+
+module.exports = {
+  ...(typeof add !== 'undefined' && { add }),
+  ...(typeof subtract !== 'undefined' && { subtract }),
+  ...(typeof multiply !== 'undefined' && { multiply }),
+  ...(typeof divide !== 'undefined' && { divide }),
+  ...(typeof logAddition !== 'undefined' && { logAddition }),
+  ...(typeof logSubtraction !== 'undefined' && { logSubtraction }),
+  ...(typeof logMultiplication !== 'undefined' && { logMultiplication }),
+  ...(typeof logDivision !== 'undefined' && { logDivision }),
+}
---
+++ test/index.test.js
@@ -0,0 +1,49 @@
+const {
+  add,
+  subtract,
+  multiply,
+  divide,
+  logAddition,
+  logSubtraction,
+  logMultiplication,
+  logDivision,
+} = require('../index')
+
+describe('Math Functions', () => {
+  const num1 = 10
+  const num2 = 5
+
+  test('add function should return correct sum', () => {})
+
+  test('subtract function should return correct difference', () => {})
+
+  test('multiply function should return correct product', () => {})
+
+  test('divide function should return correct quotient', () => {})
+
+  test('divide function should throw error when dividing by zero', () => {})
+})
+
+describe('Log Functions', () => {
+  const consoleSpy = jest.spyOn(console, 'log').mockImplementation(() => {})
+
+  afterEach(() => {
+    consoleSpy.mockClear()
+  })
+
+  afterAll(() => {
+    consoleSpy.mockRestore()
+  })
+
+  test('logAddition function should log correct addition result', () => {
+    logAddition()
+  })
+
+  test('logSubtraction function should log correct subtraction result', () => {
+    expect(consoleSpy).toHaveBeenCalledWith('Subtract: 5')
+  })
+
+  test('logMultiplication function should log correct multiplication result', () => {})
+
+  test('logDivision function should log correct division result', () => {})
+})
